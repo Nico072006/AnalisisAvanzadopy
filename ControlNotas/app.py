76e31971db5d1a4bd6e272d5a0059d7bc36a76cb
Thomasvtf <thomas.vargas899@gmail.com>
2026-04-08 07:31:35 -0500

    Nuevo 3

--- ControlNotas/app.py
+++ ControlNotas/app.py
@@ -356,4 +356,6 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Railway asigna un puerto en la variable de entorno PORT
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
